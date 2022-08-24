bf4f91fcf9528e1ef801296ce822b727a0871609
Programming Hero <programming.hero1@gmail.com>
2022-08-23 22:41:21 -0500

    explore find and filter

---
+++ filter.js
@@ -0,0 +1,17 @@
+const numbers = [12, 5, 23, 45, 11, 18, 9, 55, 61, 1];
+const bigNums = numbers.filter(number => number > 20);
+const tiny = numbers.filter(n => n < 10);
+const even = numbers.filter( num => num % 2 === 0);
+// console.log(even);
+
+const products = [
+    {id: 1, name: 'laptop', price: 45000},
+    {id: 1, name: 'mobile', price: 80000},
+    {id: 1, name: 'watch', price: 35000},
+    {id: 1, name: 'tablet', price: 23000},
+];
+
+// const expensive = products.filter(product => product.price > 100000);
+// const expensive = products.filter(product => product.price > 100);
+const expensive = products.filter(product => product.price < 100);
+console.log(expensive);
---
+++ find.js
@@ -0,0 +1,17 @@
+const numbers = [12, 5, 23, 45, 11, 18, 9, 55, 61, 1];
+
+const fives = numbers.find(num => num % 5 === 0)
+const fivesAll = numbers.filter(num => num %5 ===0);
+
+console.log(fives);
+console.log(fivesAll);
+
+const products = [
+    {id: 1, name: 'laptop', price: 65000},
+    {id: 1, name: 'mobile', price: 80000},
+    {id: 1, name: 'watch', price: 30000},
+    {id: 1, name: 'tablet', price: 23000},
+];
+
+const cheap = products.find(product => product.price < 40000);
+console.log(cheap)
